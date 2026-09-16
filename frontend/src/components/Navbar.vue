dd54419abf47a82ad8483a8d0b37f3cb2b399bcf
adityaalfarison <adityaalfarison33@gmail.com>
2026-09-17 02:39:49 +0700

    fix responsive navbar mobile

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,30 +1,38 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-white sticky-top shadow-sm">
     <div class="container py-2">
+
+      <!-- Logo + Brand -->
       <a class="navbar-brand d-flex align-items-center" href="#home">
 
-  <span class="brand-mark">
-    <img src="/logo.png" alt="Amanah Travel">
-  </span>
+        <span class="brand-mark">
+          <img src="/logo.png" alt="SAMIRA Travel">
+        </span>
 
-  <div class="brand-text">
-    <span>SAMIRA</span>
-    <u>Sahabat Umroh & Haji Keluarga Anda</u>
-  </div>
+        <div class="brand-text">
+          <span>SAMIRA</span>
+          <u>Sahabat Umroh & Haji Keluarga Anda</u>
+        </div>
 
-</a>
+      </a>
 
+      <!-- Hamburger -->
       <button
         class="navbar-toggler"
         type="button"
         data-bs-toggle="collapse"
         data-bs-target="#mainNav"
+        aria-controls="mainNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
       >
         <span class="navbar-toggler-icon"></span>
       </button>
 
+      <!-- Menu -->
       <div id="mainNav" class="collapse navbar-collapse">
         <ul class="navbar-nav ms-auto align-items-lg-center">
+
           <li class="nav-item">
             <a class="nav-link" href="#home">Beranda</a>
           </li>
@@ -46,12 +54,14 @@
           </li>
 
           <li class="nav-item ms-lg-3 mt-2 mt-lg-0">
-            <a class="btn btn-gold" href="#kontak">
+            <a class="btn btn-gold w-100" href="#kontak">
               Konsultasi
             </a>
           </li>
+
         </ul>
       </div>
+
     </div>
   </nav>
 </template>
